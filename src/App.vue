19cc5b99bd7b08ed22da5360df330335d5a60ba9
smallmonkeykey <sarinitakuoo@gmail.com>
2022-03-06 23:48:15 +0900

    Show dogs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Header from './components/Header.vue'
 import Contact from './components/Contact.vue'
-import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Header/>
   <Contact/>
-  <Footer/>
 </template>
 
 <style>
---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class="container">
+      <h2></h2>
+      
+    </div>
+  </header>
+</template>
+
+<script>
+import { ref, reactive, watch } from 'vue'
+export default {
+  setup () {
+   let msg = ref(0)
+
+  }
+}
+</script>
+
+<style scoped></style>
